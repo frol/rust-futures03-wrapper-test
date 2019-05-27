17facb19962fbf1ae7f740d547a066ff875ee37c
Vlad Frolov <frolvlad@gmail.com>
2019-05-27 23:23:08 +0300

    Added two proposed fixes

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,13 @@ struct Q;
 
 impl Q {
     fn wrapper(&self) -> Box<Future<Item = u8, Error = String>> {
+    // Fix v1: fn wrapper<'a>(&'a self) -> Box<Future<Item = u8, Error = String> + 'a> {
+    // Fix v2: fn wrapper(self: std::sync::Arc<Self>) -> Box<Future<Item = u8, Error = String>> {
         Box::new(self.inner().boxed().compat())
     }
 
     async fn inner(&self) -> Result<u8, String> {
+    // Fix v2: async fn inner(self: std::sync::Arc<Self>) -> Result<u8, String> {
         Ok(42)
     }
 }
